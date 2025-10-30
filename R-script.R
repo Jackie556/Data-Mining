e24ec7f33dfd59c9f1a218243cb05f63f03fcbfc
Md Jackie Islam <md.islam@mif.stud.vu.lt>
2025-10-30 23:27:46 +0200

    commit

--- R-script.R
+++ R-script.R
@@ -1,4 +1,37 @@
+#Start_here
+
+#LIbrary
+library(readr)
+library(caret)
+library(ggplot2)
 
 
+
+
+#main
 data<-read.csv("C:/Users/jacki/OneDrive/Desktop/Data Mining thesis/Data set/Used_Car_Price.csv")
+data
+is.na(data)
+sum(is.na(data))
+str(data)
+summary(data)
+dim(data)
+
+#data_visualizations
+# For numerical variables
+hist(data$price, main="Distribution of Price", xlab="Price")
+hist(data$enginesize, main="Engine Size Of Car",xlab="Engine Size")
+table(data$fueltype)
+hist(data$fueltype,main = "Fuel Type Of Car",xlab="Fuel Type")
+plot(density(data$horsepower), main="Horsepower Density")
+
+# For categorical variables
+table(data$fuel_type)
+barplot(table(data$body_style), main="Body Style Distribution")
+
+
+
+
+
+
 
